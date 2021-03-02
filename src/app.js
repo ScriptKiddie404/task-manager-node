623488681186c85f83ae76d9611ab581f2dd09f0
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2021-03-02 16:09:24 -0700

    post/get routes added

--- src/app.js
+++ src/app.js
@@ -13,11 +13,12 @@ const okColor = chalk.bgGreenBright.black;
 const errorColor = chalk.bgRedBright.black;
 
 
-// =========== MIDDLEWARE ============= //
+// =================== MIDDLEWARE ==================== //
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// =================================================== //
 
-app.post('/', (req, res) => {
+app.post('/users', (req, res) => {
     const user = new User(req.body);
     user.save().then(() => {
         res.status(201).send(user);
@@ -27,6 +28,26 @@ app.post('/', (req, res) => {
         console.log(errorColor(error.message));
     });
 
+});
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.status(200).send(users);
+    }).catch((error) => {
+        res.status(500).send({ error: error.message });
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    User.findById(id).then((user) => {
+        if (!user) {
+            return res.status(404).send({ error: 'User not found, lol' });
+        }
+        res.status(200).send(user);
+    }).catch((error) => {
+        res.status(500).send({ error: error.message });
+    });
 });
 
 app.post('/tasks', (req, res) => {
@@ -39,5 +60,26 @@ app.post('/tasks', (req, res) => {
         res.status(400).send({ error: error.message });
     });
 });
+
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then(tasks => {
+        res.status(200).send(tasks);
+    }).catch(error => {
+        res.status(500).send({ error: 'lol, something really bad happened!' });
+    });
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const { id } = req.params;
+    Task.findById(id).then(task => {
+        if (!task) {
+            return res.status(404).send({ error: 'there is no such task' });
+        }
+        res.status(200).send(task);
+    }).catch(error => {
+        res.status(500).send({ error: error.message });
+    })
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

a1bab2e323b150ac83a3fdc0d6ca5468f97c8b47
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2021-03-02 19:01:06 -0700

    change all routers to async/await syntax

--- src/app.js
+++ src/app.js
@@ -2,84 +2,82 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const morgan = require('morgan');
 // Imports for data models
 const User = require('./models/User');
 const Task = require('./models/Task');
 // Import for database connection
 require('./db/mongoose');
-// Unnecesary stuff just 4fun and color:
-const chalk = require('chalk');
-const okColor = chalk.bgGreenBright.black;
-const errorColor = chalk.bgRedBright.black;
-
 
 // =================== MIDDLEWARE ==================== //
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
 // =================================================== //
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
-    user.save().then(() => {
+    try {
+        await user.save();
         res.status(201).send(user);
-        console.log(okColor("User created sucessfully!"));
-    }).catch((error) => {
+
+    } catch (error) {
         res.status(400).send({ error: error.message });
-        console.log(errorColor(error.message));
-    });
-
+    }
 });
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+    const users = await User.find({});
+    try {
         res.status(200).send(users);
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send({ error: error.message });
-    });
+    }
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const { id } = req.params;
-    User.findById(id).then((user) => {
+    const user = await User.findById(id);
+    try {
         if (!user) {
-            return res.status(404).send({ error: 'User not found, lol' });
+            return res.status(404).send({ error: 'User not found' });
         }
         res.status(200).send(user);
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send({ error: error.message });
-    });
+    }
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
-    task.save().then(() => {
-        console.log(okColor("Task created successfully!"));
+    try {
+        await task.save();
         res.status(201).send(task);
-    }).catch((error) => {
-        console.log(errorColor(error.message));
+    } catch (error) {
         res.status(400).send({ error: error.message });
-    });
+    }
 });
 
-
-app.get('/tasks', (req, res) => {
-    Task.find({}).then(tasks => {
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({});
         res.status(200).send(tasks);
-    }).catch(error => {
-        res.status(500).send({ error: 'lol, something really bad happened!' });
-    });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
 });
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const { id } = req.params;
-    Task.findById(id).then(task => {
+    try {
+        const task = await Task.findById(id);
         if (!task) {
-            return res.status(404).send({ error: 'there is no such task' });
+            res.status(404).send({ error: 'Task not found' });
         }
         res.status(200).send(task);
-    }).catch(error => {
+    } catch (error) {
         res.status(500).send({ error: error.message });
-    })
-})
+    }
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/examples/mongodb.js
+++ src/examples/mongodb.js
@@ -1,141 +1,162 @@
-// Algunas dependencias para dar estilo:
-const chalk = require('chalk');
-const okColor = chalk.bgGreenBright.black;
-const errorColor = chalk.bgRedBright.black;
+// // Algunas dependencias para dar estilo:
+// const chalk = require('chalk');
+// const okColor = chalk.bgGreenBright.black;
+// const errorColor = chalk.bgRedBright.black;
 
-// Vamos a realizar un CRUD básico para MongoDB:
-// 1) Importamos mongodb (previamente instalado) -> npm i mongodb
-// Alternativa: const mongodb = require('mongodb')
-// Luego → const MongoClient = mongodb.MongoClient:
-const { MongoClient } = require('mongodb');
+// // Vamos a realizar un CRUD básico para MongoDB:
+// // 1) Importamos mongodb (previamente instalado) -> npm i mongodb
+// // Alternativa: const mongodb = require('mongodb')
+// // Luego → const MongoClient = mongodb.MongoClient:
+// const { MongoClient } = require('mongodb');
 
-// 2) Definimos los datos que vamos a utilizar
-const URL = 'mongodb://localhost:27017';
-const databaseName = 'task-manager';
+// // 2) Definimos los datos que vamos a utilizar
+// const URL = 'mongodb://localhost:27017';
+// const databaseName = 'task-manager';
 
-// 3) Usamos el cliente mongo para conectar
-// Parámetros: (URL, objecto, callback)
-MongoClient.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+// // 3) Usamos el cliente mongo para conectar
+// // Parámetros: (URL, objecto, callback)
+// MongoClient.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
 
-    if (error) {
-        throw error;
-    }
+//     if (error) {
+//         throw error;
+//     }
 
-    console.log(okColor(" Connected correctly! 😀 "));
+//     console.log(okColor(" Connected correctly! 😀 "));
 
-    // 4) creamos la base de datos:
-    const db = client.db(databaseName);
+//     // 4) creamos la base de datos:
+//     const db = client.db(databaseName);
 
-    // // Para buscar por id:
-    // const ObjectID = require('mongodb');
-    // db.collection('users').findOne({_id: new ObjectID('a87d08as7d08as')})...etc
-
-
-
-});
+//     // // Para buscar por id:
+//     // const ObjectID = require('mongodb');
+//     // db.collection('users').findOne({_id: new ObjectID('a87d08as7d08as')})...etc
 
 
-// Realizar una consulta:
-db.collection('users').findOne({ name: 'Diana' }, (error, result) => {
+
+// });
+
+
+// // Realizar una consulta:
+// db.collection('users').findOne({ name: 'Diana' }, (error, result) => {
+//     console.log(result);
+// });
+
+// // Realizar una consulta que devuelva muchos documentos bajo 1 criterio.
+
+// db.collection('users').find({ age: 11 }).toArray((error, result) => {
+//     console.log(result);
+// });
+
+// // Devolver absolutamnente todo:
+// db.collection('users').find({}).toArray((error, result) => {
+//     console.log(result);
+// });
+
+// db.collection('users').insertOne({
+//     name: 'John Doe',
+//     age: 18
+// }, (error, result) => {
+//     if (error) {
+//         return console.log(errorColor(' Unable to insert user, you stayed: 🤡 '));
+//     }
+//     console.log(result.ops);
+// });
+
+// // Un ejemplo de insertMany:
+// db.collection('users').insertMany([
+//     {
+//         name: "Fernando",
+//         age: 11
+//     },
+//     {
+//         name: "Diana",
+//         age: 30
+//     },
+//     {
+//         name: "Jaja",
+//         age: 70
+//     }
+// ], (error, result) => {
+//     if (error) {
+//         return console.log(errorColor(' Unable to insert user, you stayed: 🤡 '));
+//     }
+//     console.log(okColor(`${result.insertedCount} documents successfully inserted 😍 `));
+// });
+
+
+
+// db.collection('tasks').insertMany([
+//     {
+//         description: 'This is some random description #1',
+//         completed: true
+//     },
+//     {
+//         description: 'This is some random description #2',
+//         completed: true
+//     },
+//     {
+//         description: 'This is some random description #3',
+//         completed: true
+//     }
+// ], (error, result) => {
+//     if (error) {
+//         return console.log(errorColor(' Unable to insert tasks, you stayed: 🤡 '))
+//     }
+
+//     console.log(okColor(`${result.insertedCount} documents successfully inserted 😍 `))
+//     console.log(okColor(result.ops));
+
+// });
+
+
+
+// // Ejemplo creando una instancia del modelo:
+// const yo = new User({
+//     name: 'Raymond',
+//     email: 'jejeputos@gmail.com',
+//     password: 'pedro12password',
+//     age: 22
+// });
+
+// // Guardar dicha instancia en la DB (prmise version)
+// yo.save().then(() => {
+//     console.log(yo);
+// }).catch(error => {
+//     console.log(error);
+// });
+
+
+// // Creando una instancia de dicha tarea:
+// const washDishes = new Task({
+//     description: "Wash dishes, lol?",
+//     completed: false
+// });
+
+// // Guardamos en la base:
+// washDishes.save().then(() => {
+//     console.log(washDishes)
+// }).catch(error => {
+//     console.log(error);
+// });
+
+
+
+// Async await examples
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b);
+        }, 2000);
+    });
+}
+
+const foo = async () => {
+    const sum = await add(2, 5);
+    return sum;
+}
+
+foo().then(result => {
     console.log(result);
-});
-
-// Realizar una consulta que devuelva muchos documentos bajo 1 criterio.
-
-db.collection('users').find({ age: 11 }).toArray((error, result) => {
-    console.log(result);
-});
-
-// Devolver absolutamnente todo:
-db.collection('users').find({}).toArray((error, result) => {
-    console.log(result);
-});
-
-db.collection('users').insertOne({
-    name: 'John Doe',
-    age: 18
-}, (error, result) => {
-    if (error) {
-        return console.log(errorColor(' Unable to insert user, you stayed: 🤡 '));
-    }
-    console.log(result.ops);
-});
-
-// Un ejemplo de insertMany:
-db.collection('users').insertMany([
-    {
-        name: "Fernando",
-        age: 11
-    },
-    {
-        name: "Diana",
-        age: 30
-    },
-    {
-        name: "Jaja",
-        age: 70
-    }
-], (error, result) => {
-    if (error) {
-        return console.log(errorColor(' Unable to insert user, you stayed: 🤡 '));
-    }
-    console.log(okColor(`${result.insertedCount} documents successfully inserted 😍 `));
-});
-
-
-
-db.collection('tasks').insertMany([
-    {
-        description: 'This is some random description #1',
-        completed: true
-    },
-    {
-        description: 'This is some random description #2',
-        completed: true
-    },
-    {
-        description: 'This is some random description #3',
-        completed: true
-    }
-], (error, result) => {
-    if (error) {
-        return console.log(errorColor(' Unable to insert tasks, you stayed: 🤡 '))
-    }
-
-    console.log(okColor(`${result.insertedCount} documents successfully inserted 😍 `))
-    console.log(okColor(result.ops));
-
-});
-
-
-
-// Ejemplo creando una instancia del modelo:
-const yo = new User({
-    name: 'Raymond',
-    email: 'jejeputos@gmail.com',
-    password: 'pedro12password',
-    age: 22
-});
-
-// Guardar dicha instancia en la DB (prmise version)
-yo.save().then(() => {
-    console.log(yo);
 }).catch(error => {
     console.log(error);
-});
-
-
-// Creando una instancia de dicha tarea:
-const washDishes = new Task({
-    description: "Wash dishes, lol?",
-    completed: false
-});
-
-// Guardamos en la base:
-washDishes.save().then(() => {
-    console.log(washDishes)
-}).catch(error => {
-    console.log(error);
-});
-
-////////// NEXT → 7
+})
